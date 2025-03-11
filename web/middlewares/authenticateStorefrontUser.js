3fd211965da0b5ff244fb4e19e90b0b227b0d662
zenmeraki7 <zenmerakihelp@gmail.com>
2025-03-11 16:05:55 +0530

    Abhishek branch

--- web/middlewares/authenticateStorefrontUser.js
+++ web/middlewares/authenticateStorefrontUser.js
@@ -1,12 +1,16 @@
 import shopify from "../shopify.js"
 
 export const authenticateUser = async(req,res,next) => {
+    console.log("is working.");
+    
     try{
         let shop = req.query.shop
         let store = await shopify.config.sessionStorage.findSessionsByShop(shop)
         
         if(shop === store[0].shop){
             req.session = store[0]
+            console.log("working...");
+            
             next()
         }else{
             res.status(401).json({message:"Unauthorized"})
